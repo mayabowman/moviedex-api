3dd7e068ffd2dbe24dc845f0322ebf53fd17d3be
Maya Bowman <mayashady1@gmail.com>
2019-10-16 16:06:26 -0400

    added movie endpoint

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
+console.log(process.env.API_TOKEN)
+
 app.use(morgan('dev'))
 
-app.use((req, res) => {
-  res.send('Hello, world!')
+app.use(function validateBearerToken(req, res, next) {
+  console.log('validate bearer token middleware')
+  // move to the next middleware
+  next()
 })
+
+app.get('/movie', handleGetMovie)
+
+function handleGetMovie(req, res) {
+  res.send('Hello, Hollywood!')
+}
 
 const PORT = 8000
 
