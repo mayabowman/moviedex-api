8059e762b4aa644dafa1a528b768b6729d1ac2e5
Maya Bowman <mayashady1@gmail.com>
2019-10-16 16:44:59 -0400

    added filters

--- server.js
+++ server.js
@@ -1,23 +1,54 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const helmet = require('helmet')
+const cors = require('cors')
 const app = express()
+const MOVIEDEX = require('./moviedex.json')
 
 console.log(process.env.API_TOKEN)
 
 app.use(morgan('dev'))
+app.use(helmet())
+app.use(cors())
 
 app.use(function validateBearerToken(req, res, next) {
-  console.log('validate bearer token middleware')
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+  
+  if(!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
   // move to the next middleware
   next()
 })
 
-app.get('/movie', handleGetMovie)
+app.get('/movie', function handleGetMovie(req, res) {
+  let response = MOVIEDEX
 
-function handleGetMovie(req, res) {
-  res.send('Hello, Hollywood!')
-}
+  // filter by genre
+  if(req.query.genre) {
+    response = response.filter(movie =>
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())  
+    )
+  }
+
+  // filter by country
+  if(req.query.country) {
+    response = response.filter(movie =>
+      movie.country.toLowerCase().includes(req.query.country.toLowerCase())  
+    )
+  }
+
+  // filter by average vote
+  if(req.query.avg_vote) {
+    response = response.filter(movie =>
+      Number(movie.avg_vote) >= Number(req.query.avg_vote)
+    )
+  }
+
+  res.json(response)
+})
 
 const PORT = 8000
 
